2341cad175741d8e9edfe6c406afdd0381d762d5
Brooke Bachman <bbachman1994@gmail.com>
2019-11-14 22:25:54 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,12 +21,6 @@ def long_planeteer_calls(array)
   end
 end
 
-
-
-  # code an argument here
-  # Your code here
-end
-
 def find_the_cheese(array)# code an argument here
   # the array below is here to help
   new_array = []
