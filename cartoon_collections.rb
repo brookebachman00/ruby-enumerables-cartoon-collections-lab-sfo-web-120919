148b43644d4d81b120058aaf381d6c6df1310fb6
Brooke Bachman <bbachman1994@gmail.com>
2019-11-14 22:19:09 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,7 +8,7 @@ end
 
 def summon_captain_planet(planeteer_calls)
   planeteer_calls.map do |thing|
-    puts thing[0].upcase + "!"
+    puts "#{thing.upcase} + '!'"
   end
   # code an argument here
   # Your code here
