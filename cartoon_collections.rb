1a682b3cde556c365c2738a5875850c540e159e7
Brooke Bachman <bbachman1994@gmail.com>
2019-11-14 21:58:56 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,4 +1,8 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(array)
+  array.each_with_index |dwarf,index| do
+    puts dwarf,index
+  end
+end# code an argument here
   # Your code here
 end
 
