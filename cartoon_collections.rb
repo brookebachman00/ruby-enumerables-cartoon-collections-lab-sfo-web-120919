f6cb7c5b8dc15beb6fa4a05e1e57c3b08ac77cd1
Brooke Bachman <bbachman1994@gmail.com>
2019-11-14 22:08:53 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,13 +5,13 @@ def roll_call_dwarves(array)
   end
 end
 
-def summon_captain_planet(planeteer_calls)
-  planeteer_calls.map |thing| do
-    puts "#{thing}.upcase + '!'"
-  end
-  # code an argument here
-  # Your code here
-end
+# def summon_captain_planet(planeteer_calls)
+#   planeteer_calls.map |thing| do
+#     puts "#{thing}.upcase + '!'"
+#   end
+#   # code an argument here
+#   # Your code here
+# end
 
 def long_planeteer_calls# code an argument here
   # Your code here
